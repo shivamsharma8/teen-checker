6f72f5b04937124f828939af0b8cb0528533e934
shivamsharma8 <113785642+shivamsharma8@users.noreply.github.com>
2022-09-30 21:41:22 +0530

    Add files via upload

---
+++ TeenNumberChecker.java
@@ -0,0 +1,11 @@
+public class TeenNumberChecker {
+
+    public static boolean hasTeen( int a , int b, int c) {
+
+        return (a <= 19 && a >= 13) || (b <= 19 && b >= 13) || (c <= 19 && c >= 13);
+    }
+
+    public static boolean isTeen(int d){
+        return d <= 19 && d >= 13;
+    }
+}
